a9564bc4a474326770efd2bb169430d8791db799
Surajman <suraj.mce.dtu@gmail.com>
2019-11-25 07:24:49 +0530

    Updates

---
+++ precache-manifest.2a6fb06e0f4cef032f5a169fbe5e9fe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f063605d9129f9a47796c0185bc3ae2d",
+    "url": "surajman.github.io/imdb-helper/index.html"
+  },
+  {
+    "revision": "4d498378e2f137abd2ce",
+    "url": "surajman.github.io/imdb-helper/static/css/main.add3e49b.chunk.css"
+  },
+  {
+    "revision": "966e73be97d66daa7329",
+    "url": "surajman.github.io/imdb-helper/static/js/2.0f8770c3.chunk.js"
+  },
+  {
+    "revision": "4d498378e2f137abd2ce",
+    "url": "surajman.github.io/imdb-helper/static/js/main.e4c19e09.chunk.js"
+  },
+  {
+    "revision": "cdd5aeeae8087ba4e002",
+    "url": "surajman.github.io/imdb-helper/static/js/runtime-main.95e9ab45.js"
+  }
+]);
